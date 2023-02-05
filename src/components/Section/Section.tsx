d8f0cfefe7ef30affd96e9443eca01fb7208cb9c
Ben Blayer <betrrdev@gmail.com>
2023-02-06 01:18:34 +0200

    minor change

--- src/components/Section/Section.tsx
+++ src/components/Section/Section.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Section: React.FC = () => {
     return (
-        <section id="comp-l9ac4b96" className='db-section' tabIndex={-1} />
+        <section className='db-section' tabIndex={-1} />
     );
 };
 
