e320959e49391f2e8d0145392e9d755ab1c1f63c
Ben Blayer <betrrdev@gmail.com>
2023-02-06 01:15:37 +0200

    add bootstrap and sass

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import './App.css';
+import './App.scss';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 import HomePage from './components/HomePage/HomePage';
