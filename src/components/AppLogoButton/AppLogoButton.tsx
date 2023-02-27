46a8b46d794a94b52ae499e3cf08a6cfce655a70
Ben Blayer <betrrdev@gmail.com>
2023-02-27 23:17:48 +0200

    add brand & logo

--- src/components/AppLogoButton/AppLogoButton.tsx
+++ src/components/AppLogoButton/AppLogoButton.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 import { AppLogoButtonProps } from '../../@types/types';
 
 const AppLogoButton: React.FC<AppLogoButtonProps> = ({ minified }) => {
-  const src = 'clickClassLogoSmall.png';
-  const height = '55px';
-  const width = '57px';
+  const src = 'sylaboo.png';
+  const height = '75px';
+  const width = '77px';
   return (
     <a href='/'>
       <img src={src} alt='Brand Logo' height={height} width={width} />
